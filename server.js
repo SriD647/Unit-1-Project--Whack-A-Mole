b6837ac7f3f7b1e88e59ddebf46ae731885a4132
SriD647 <sridatt.rao@hotmail.com>
2023-08-25 19:19:38 -0400

    deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8083;
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
